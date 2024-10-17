8052b88446e7b5cc1b98c02897c29b15bb72b69d
Brennan Hill <brehill@sandia.gov>
2024-10-17 09:10:08 -0600

    Initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# x86-assembly-comments
+vscode extension to highlight // comments in x86 assembly files.
---
+++ test.asm
@@ -0,0 +1,7 @@
+// aaa
+main:
+    // this does absolutely nothing useful
+    ; here's another comment
+    mov eax, 1 // this also does something
+    mov rax, rbx
+    add rax, 1
